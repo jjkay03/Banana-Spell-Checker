f82b256ca80bf3c075f47f42c1c3d2da333414b5
Thierrix <thierry.sokhn@epfl.ch>
2024-03-14 00:02:04 +0100

    Edited spell_checker

--- modules/spell_checker.py
+++ modules/spell_checker.py
@@ -4,6 +4,11 @@
 
 # By Armurier & jjkay03 - Started on 13/02/2024
 
+from .logger import log
+
+from textblob import TextBlob
+
+
 def spell_checker(text):
     """
     Function taking a string as input and returning the same string without any spelling mistakes
@@ -12,4 +17,17 @@ def spell_checker(text):
         text (str): The text to check
     """
 
+    log("Entered spell checking text", debug=False)
     
+    spell_checked_text = TextBlob(text).correct()
+
+    log("Spell checked text and exiting the function", debug=False)
+
+
+    """
+    This version kind of works, however it is not perfect. It is not able to correct the following sentence:
+    "I hate peple who kan't spel." ---> does not correct "kan't" to "can't" but "kan" to "an't".
+
+    We also need to check if we can change the language of the spell checker to French, or let the choice to the user.
+    """
+    return spell_checked_text
